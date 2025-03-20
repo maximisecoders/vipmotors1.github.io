d920248b96dcbb1e7d3f3cc43d7a4678096d46aa
maximisecoders <maxproductionsofficials@gmail.com>
2025-03-21 05:17:00 +0530

    Delete src directory

--- src/CarListing.tsx
+++
@@ -1,9 +0,0 @@
-const CarListing = ({ car }) => {
-  return (
-    <div className="car-listing">
-      <p className="price">Price: {car.price} AED</p>
-    </div>
-  );
-};
-
-export default CarListing;
